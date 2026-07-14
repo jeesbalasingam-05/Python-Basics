a54222c1b13ca44ec499d40123499ee21c2e11ec
jeesbalasingam-05 <jeesbalasingam05@gmail.com>
2026-07-14 19:14:11 +0530

    Add files via upload
    These are the basics problems of python

---
+++ PT5.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter starting number:"))
+num2=int(input("Enter Ending number:"))
+sum=0
+
+for i in range(num1,num2+1):
+    sum=sum+i
+
+print(sum)
+    
---
+++ PT6.py
@@ -0,0 +1,11 @@
+A=int(input("Enter A:"))
+B=int(input("Enter B:"))
+
+if A>B:
+    print("A is greater")
+
+elif B>A:
+    print("B is greater")
+
+else:
+    print("A and B are equal")
